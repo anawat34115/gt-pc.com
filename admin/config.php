b46bc2abeb4fbaa9dfc84f8959bea0919abb0616
anawat34115 <85146954+anawat34115@users.noreply.github.com>
2025-09-21 17:07:49 +0700

    fix location file toserver

--- admin/config.php
+++ admin/config.php
@@ -3,5 +3,8 @@
 $ADMIN_USER = "admin";
 $ADMIN_PASS = "1234";
 
-// ใช้ session
+// กำหนดโฟลเดอร์เก็บ session ให้เป็น tmp ในเว็บเรา
+session_save_path(__DIR__ . '/tmp');
+
+// เริ่ม session
 session_start();
