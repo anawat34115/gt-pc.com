02fe015f524751b14caf4daba42e73e932dcf760
anawat34115 <85146954+anawat34115@users.noreply.github.com>
2025-10-29 18:18:28 +0700

    fix page

--- js/main.js
+++ js/main.js
@@ -172,7 +172,7 @@ if (langButtons.length) {
       const lang = btn.dataset.lang;
       let target = '/index.html';
       if (lang === 'en') target = '/en/index.html';
-      if (lang === 'cn') target = '/cn/index.html';
+      if (lang === 'zh') target = '/zh/index.html';
 
       // ปิดเมนูแล้วค่อยนำทาง
       const mobileMenu = navbarPlaceholder.querySelector('#mobile-menu');
